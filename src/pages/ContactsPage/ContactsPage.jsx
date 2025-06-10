a8ad4418753ad9b5795348163c9c605026ded602
Andriy Ivanchenko <mais-nasinya@ukr.net>
2025-06-10 12:35:24 +0300

    Andriy

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -25,10 +25,6 @@ export default function ContactsPage() {
   return (
     <>
       <PageTitle>Your contacts</PageTitle>
-      <ContactForm />
-      {isLoading && <p>Loading contacts...</p>}
-      {error && <p>Error: {error}</p>}
-      <ContactList contacts={contacts} />
     </>
   );
 }
